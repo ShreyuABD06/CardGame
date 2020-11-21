e07b1955700658c0d2182a105512d56a08482718
ShreyuABD06 <74808159+ShreyuABD06@users.noreply.github.com>
2020-11-21 11:13:11 +0530

    Add files via upload

---
+++ GameOfCards/Program.cs
@@ -0,0 +1,22 @@
+﻿using GameOfCards.BLL;
+using GameOfCards.Interfaces;
+using System;
+
+namespace GameOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                INewGame newGame = new NewGame();
+                newGame.SetupNewGame();
+            }catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+        }
+    }
+}
